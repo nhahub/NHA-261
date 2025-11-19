49211378a888c9742d923933645c0d7e66c7cbb8
hazemhamdy10 <hazemhamdyhafez@gmai.com>
2025-11-20 00:12:38 +0200

    this is Hazem_2nd_update

--- SSS/STOCKUPMVC/STOCKUPMVC/Models/Product.cs
+++ SSS/STOCKUPMVC/STOCKUPMVC/Models/Product.cs
@@ -1,30 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using STOCKUPMVC.Models;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace STOCKUPMVC.Models
+public class Product
 {
-    public class Product
+    public Product()
     {
-        [Key]
-        public int ProductID { get; set; }
-
-        [Required]
-        public string Name { get; set; }
-
-        public string SKU { get; set; }
-        public string Description { get; set; }
-
-        [ForeignKey("Category")]
-        public int CategoryID { get; set; }
-
-        public decimal Price { get; set; }
-
-        // Navigation properties
-        public Category Category { get; set; }
-        public ICollection<Inventory> Inventories { get; set; }
-        public ICollection<OrderItem> OrderItems { get; set; }
-        public ICollection<SupplierProduct> SupplierProducts { get; set; }
-        public ICollection<StockMovement> StockMovements { get; set; }
+        Inventories = new List<Inventory>();
+        OrderItems = new List<OrderItem>();
+        SupplierProducts = new List<SupplierProduct>();
+        StockMovements = new List<StockMovement>();
     }
+
+    [Key]
+    public int ProductID { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    public string SKU { get; set; }
+    public string Description { get; set; }
+
+    [ForeignKey("Category")]
+    public int CategoryID { get; set; }
+
+    public decimal Price { get; set; }
+
+    // Navigation properties
+    public Category Category { get; set; }
+    public ICollection<Inventory> Inventories { get; set; }
+    public ICollection<OrderItem> OrderItems { get; set; }
+    public ICollection<SupplierProduct> SupplierProducts { get; set; }
+    public ICollection<StockMovement> StockMovements { get; set; }
 }
